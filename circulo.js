5c7c7f75a1461b43fb4daea868b867bfb801e4e6
Renata Gardenia <121052380+rehgardenia@users.noreply.github.com>
2023-08-22 21:25:05 -0300

    Create circulo.js

---
+++ circulo.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500,500);
+  background("lightblue");
+}
+
+function draw(){
+
+  fill("yellow");
+  circle(250,250,300);   // x, y , diametro
+
+  fill("darkblue");
+  rect(0,300,500,350);  // x, y, largura, altura
+ 
+  fill("khaki");
+  rect(0,400,500,100);
+}
